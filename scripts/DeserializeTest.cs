cc90fff93363b324b13655d46d59848a7a1f177a
Julian Pompey <julianpomp00@gmail.com>
2025-08-26 19:09:28 -0400

    first commit

---
+++ scripts/DeserializeTest.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+using System.Text.Json;
+
+public partial class DeserializeTest : Node2D
+{
+
+    public override void _Ready()
+    {
+        deserialize();
+    }
+
+
+    public override void _Process(double delta)
+    {
+
+    }
+
+    public void deserialize()
+    {
+        String filePath = "resources\\gurps_character_sheet.json";
+        FileAccess file = FileAccess.Open(filePath, FileAccess.ModeFlags.Read);
+        if (file == null)
+        {
+            return;
+        }
+
+        string jsonText = file.GetAsText();
+        file.Close();
+        // GD.Print(jsonText);
+        // JsonSerializer.Deserialize<GurpsSheetModel>(jsonText);
+
+        //IT WORKS??
+        GurpsSheetModel gurpsSheetModel = JsonSerializer.Deserialize<GurpsSheetModel>(jsonText);
+        // GD.Print(gurpsSheetModel.stats.fatigue_points.stat);
+    }
+}
